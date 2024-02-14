31810af9ebda9175e58a6fe0e0097d43d68a5687
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-02-14 14:15:55 +0300

    - update code

---
+++ src/Nethereum.Web3.Wrappers/ContractHandlerWrapper.cs
@@ -0,0 +1,105 @@
+﻿using Nethereum.Contracts;
+using Nethereum.ABI.Decoders;
+using Nethereum.Hex.HexTypes;
+using Nethereum.RPC.Eth.DTOs;
+using Nethereum.Contracts.Services;
+using Nethereum.Contracts.ContractHandlers;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+
+namespace Nethereum.Web3.Wrappers;
+
+public class ContractHandlerWrapper
+{
+    private readonly ContractHandler contractHandler;
+    public virtual string ContractAddress => contractHandler.ContractAddress;
+    public virtual EthApiContractService EthApiContractService => contractHandler.EthApiContractService;
+
+    public ContractHandlerWrapper(ContractHandler contractHandler)
+    {
+        this.contractHandler = contractHandler;
+    }
+
+    public virtual Event<TEventType> GetEvent<TEventType>()
+        where TEventType : IEventDTO, new()
+    {
+        return contractHandler.GetEvent<TEventType>();
+    }
+
+    public virtual Function<TEthereumContractFunctionMessage> GetFunction<TEthereumContractFunctionMessage>()
+        where TEthereumContractFunctionMessage : new()
+    {
+        return contractHandler.GetFunction<TEthereumContractFunctionMessage>();
+    }
+
+    public virtual Task<TransactionReceipt> SendRequestAndWaitForReceiptAsync<TEthereumContractFunctionMessage>(
+        TEthereumContractFunctionMessage? transactionMessage = null, CancellationTokenSource? tokenSource = null)
+        where TEthereumContractFunctionMessage : FunctionMessage, new()
+    {
+        return contractHandler.SendRequestAndWaitForReceiptAsync(transactionMessage, tokenSource);
+    }
+
+    public virtual Task<TransactionReceipt> SendRequestAndWaitForReceiptAsync<TEthereumContractFunctionMessage>(
+        TEthereumContractFunctionMessage transactionMessage, CancellationToken cancellationToken)
+        where TEthereumContractFunctionMessage : FunctionMessage, new()
+    {
+        return contractHandler.SendRequestAndWaitForReceiptAsync(transactionMessage, cancellationToken);
+    }
+
+    public virtual Task<string> SendRequestAsync<TEthereumContractFunctionMessage>(
+        TEthereumContractFunctionMessage? transactionMessage = null)
+        where TEthereumContractFunctionMessage : FunctionMessage, new()
+    {
+        return contractHandler.SendRequestAsync(transactionMessage);
+    }
+
+    public virtual Task<string> SignTransactionAsync<TEthereumContractFunctionMessage>(
+        TEthereumContractFunctionMessage? transactionMessage = null)
+        where TEthereumContractFunctionMessage : FunctionMessage, new()
+    {
+        return contractHandler.SignTransactionAsync(transactionMessage);
+    }
+
+    public virtual Task<HexBigInteger> EstimateGasAsync<TEthereumContractFunctionMessage>(
+        TEthereumContractFunctionMessage? transactionMessage = null)
+        where TEthereumContractFunctionMessage : FunctionMessage, new()
+    {
+        return contractHandler.EstimateGasAsync(transactionMessage);
+    }
+
+    public virtual Task<TEthereumFunctionReturn> QueryDeserializingToObjectAsync<TEthereumContractFunctionMessage, TEthereumFunctionReturn>(
+        TEthereumContractFunctionMessage? ethereumContractFunctionMessage = null, BlockParameter? blockParameter = null)
+        where TEthereumContractFunctionMessage : FunctionMessage, new()
+        where TEthereumFunctionReturn : IFunctionOutputDTO, new()
+    {
+        return contractHandler.QueryDeserializingToObjectAsync<TEthereumContractFunctionMessage, TEthereumFunctionReturn>(ethereumContractFunctionMessage, blockParameter);
+    }
+
+    public virtual Task<TReturn> QueryAsync<TEthereumContractFunctionMessage, TReturn>(
+        TEthereumContractFunctionMessage? ethereumContractFunctionMessage = null, BlockParameter? blockParameter = null)
+        where TEthereumContractFunctionMessage : FunctionMessage, new()
+    {
+        return contractHandler.QueryAsync<TEthereumContractFunctionMessage, TReturn>(ethereumContractFunctionMessage, blockParameter);
+    }
+
+    public virtual Task<byte[]> QueryRawAsync<TEthereumContractFunctionMessage>(
+        TEthereumContractFunctionMessage? ethereumContractFunctionMessage = null, BlockParameter? blockParameter = null)
+        where TEthereumContractFunctionMessage : FunctionMessage, new()
+    {
+        return contractHandler.QueryRawAsync(ethereumContractFunctionMessage, blockParameter);
+    }
+
+    public virtual Task<TReturn> QueryRawAsync<TEthereumContractFunctionMessage, TCustomDecoder, TReturn>(BlockParameter? blockParameter = null)
+        where TEthereumContractFunctionMessage : FunctionMessage, new()
+        where TCustomDecoder : ICustomRawDecoder<TReturn>, new()
+    {
+        return contractHandler.QueryRawAsync<TEthereumContractFunctionMessage, TCustomDecoder, TReturn>(blockParameter);
+    }
+
+    public virtual Task<TReturn> QueryRawAsync<TEthereumContractFunctionMessage, TCustomDecoder, TReturn>(
+        TEthereumContractFunctionMessage ethereumContractFunctionMessage, BlockParameter? blockParameter = null)
+        where TEthereumContractFunctionMessage : FunctionMessage, new()
+        where TCustomDecoder : ICustomRawDecoder<TReturn>, new()
+    {
+        return contractHandler.QueryRawAsync<TEthereumContractFunctionMessage, TCustomDecoder, TReturn>(ethereumContractFunctionMessage, blockParameter);
+    }
+}
--- src/Nethereum.Web3.Wrappers/ERC20ContractServiceWrapper.cs
+++ src/Nethereum.Web3.Wrappers/ERC20ContractServiceWrapper.cs
@@ -1,207 +1,168 @@
 ﻿using System.Numerics;
+using Nethereum.Contracts;
 using Nethereum.RPC.Eth.DTOs;
 using Nethereum.Contracts.Standards.ERC20;
-using Nethereum.Contracts.Standards.ERC20.ContractDefinition;
 using Nethereum.Contracts.ContractHandlers;
-using Nethereum.Contracts.Services;
+using Nethereum.Contracts.Standards.ERC20.ContractDefinition;
 
 namespace Nethereum.Web3.Wrappers;
 
-/// <summary>
-/// Provides a wrapper around the <see cref="ERC20ContractService"/> to interact with ERC20 tokens on the Ethereum blockchain.
-/// </summary>
-/// <remarks>
-/// This wrapper simplifies the usage of the ERC20 contract service by abstracting away some of the complexities involved in blockchain transactions.
-/// </remarks>
 public class ERC20ContractServiceWrapper
 {
-    protected readonly ERC20ContractService contractService;
+    private readonly ERC20ContractService contractService;
 
-    public ERC20ContractServiceWrapper(IWeb3 web3, string contractAddress)
-    {
-        this.contractService = web3.Eth.ERC20.GetContractService(contractAddress);
-    }
-
-    public ERC20ContractServiceWrapper(IEthApiContractService eth, string contractAddress)
-    {
-        this.contractService = eth.ERC20.GetContractService(contractAddress);
-    }
-
-    public ERC20ContractServiceWrapper(ERC20Service erc20, string contractAddress)
-    {
-        this.contractService = erc20.GetContractService(contractAddress);
-    }
+    public virtual string ContractAddress => contractService.ContractAddress;
+    public virtual ContractHandler ContractHandler => contractService.ContractHandler;
 
     public ERC20ContractServiceWrapper(ERC20ContractService contractService)
     {
         this.contractService = contractService;
     }
 
-    /// <inheritdoc cref="ERC20ContractService.ContractAddress"/>
-    public virtual string ContractAddress => contractService.ContractAddress;
-
-    /// <inheritdoc cref="ERC20ContractService.ContractHandler"/>
-    public virtual ContractHandler ContractHandler => contractService.ContractHandler;
-
-    /// <inheritdoc cref="ERC20ContractService.TotalSupplyQueryAsync(TotalSupplyFunction, BlockParameter)"/>
-    public virtual async Task<BigInteger> TotalSupplyQueryAsync(TotalSupplyFunction totalSupplyFunction, BlockParameter? blockParameter = null)
+    public virtual Event<ApprovalEventDTO> GetApprovalEvent()
     {
-        return await contractService.TotalSupplyQueryAsync(totalSupplyFunction, blockParameter);
+        return contractService.GetApprovalEvent();
     }
 
-    /// <inheritdoc cref="ERC20ContractService.TotalSupplyQueryAsync(BlockParameter)"/>
-    public virtual async Task<BigInteger> TotalSupplyQueryAsync(BlockParameter? blockParameter = null)
+    public virtual Event<TransferEventDTO> GetTransferEvent()
     {
-        return await contractService.TotalSupplyQueryAsync(blockParameter);
+        return contractService.GetTransferEvent();
     }
 
-    /// <inheritdoc cref="ERC20ContractService.DecimalsQueryAsync(DecimalsFunction, BlockParameter)"/>
-    public virtual async Task<byte> DecimalsQueryAsync(DecimalsFunction decimalsFunction, BlockParameter? blockParameter = null)
+    public virtual Task<string> NameQueryAsync(NameFunction? nameFunction = null, BlockParameter? blockParameter = null)
     {
-        return await contractService.DecimalsQueryAsync(decimalsFunction, blockParameter);
+        return contractService.NameQueryAsync(nameFunction, blockParameter);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.DecimalsQueryAsync(BlockParameter)"/>
-    public virtual async Task<byte> DecimalsQueryAsync(BlockParameter? blockParameter = null)
+    public virtual Task<string> SymbolQueryAsync(SymbolFunction? symbolFunction = null, BlockParameter? blockParameter = null)
     {
-        return await contractService.DecimalsQueryAsync(blockParameter);
+        return contractService.SymbolQueryAsync(symbolFunction, blockParameter);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.NameQueryAsync(NameFunction, BlockParameter)"/>
-    public virtual async Task<string> NameQueryAsync(NameFunction? nameFunction = null, BlockParameter? blockParameter = null)
+    public virtual Task<string> ApproveRequestAsync(ApproveFunction approveFunction)
     {
-        return await contractService.NameQueryAsync(nameFunction, blockParameter);
+        return contractService.ApproveRequestAsync(approveFunction);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.SymbolQueryAsync(SymbolFunction, BlockParameter)"/>
-    public virtual async Task<string> SymbolQueryAsync(SymbolFunction symbolFunction, BlockParameter? blockParameter = null)
+    public virtual Task<string> ApproveRequestAsync(string spender, BigInteger value)
     {
-        return await contractService.SymbolQueryAsync(symbolFunction, blockParameter);
+        return contractService.ApproveRequestAsync(spender, value);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.ApproveRequestAsync(ApproveFunction)"/>
-    public virtual async Task<string> ApproveRequestAsync(ApproveFunction approveFunction)
+    public virtual Task<TransactionReceipt> ApproveRequestAndWaitForReceiptAsync(ApproveFunction approveFunction, CancellationToken cancellationToken = default)
     {
-        return await contractService.ApproveRequestAsync(approveFunction);
+        return contractService.ApproveRequestAndWaitForReceiptAsync(approveFunction, cancellationToken);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.ApproveRequestAsync(string, BigInteger)"/>
-    public virtual async Task<string> ApproveRequestAsync(string spender, BigInteger value)
+    public virtual Task<TransactionReceipt> ApproveRequestAndWaitForReceiptAsync(string spender, BigInteger value, CancellationToken cancellationToken = default)
     {
-        return await contractService.ApproveRequestAsync(spender, value);
+        return contractService.ApproveRequestAndWaitForReceiptAsync(spender, value, cancellationToken);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.TransferFromRequestAsync(TransferFromFunction)"/>
-    public virtual async Task<string> TransferFromRequestAsync(TransferFromFunction transferFromFunction)
+    public virtual Task<BigInteger> TotalSupplyQueryAsync(TotalSupplyFunction totalSupplyFunction, BlockParameter? blockParameter = null)
     {
-        return await contractService.TransferFromRequestAsync(transferFromFunction);
+        return contractService.TotalSupplyQueryAsync(totalSupplyFunction, blockParameter);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.TransferFromRequestAsync(string, string, BigInteger)"/>
-    public virtual async Task<string> TransferFromRequestAsync(string from, string to, BigInteger value)
+    public virtual Task<BigInteger> TotalSupplyQueryAsync(BlockParameter? blockParameter = null)
     {
-        return await contractService.TransferFromRequestAsync(from, to, value);
+        return contractService.TotalSupplyQueryAsync(blockParameter);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.TransferRequestAsync(TransferFunction)"/>
-    public virtual async Task<string> TransferFromRequestAsync(TransferFunction transferFunction)
+    public virtual Task<string> TransferFromRequestAsync(TransferFromFunction transferFromFunction)
     {
-        return await contractService.TransferRequestAsync(transferFunction);
+        return contractService.TransferFromRequestAsync(transferFromFunction);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.TransferRequestAsync(string, BigInteger)"/>
-    public virtual async Task<string> TransferFromRequestAsync(string to, BigInteger value)
+    public virtual Task<string> TransferFromRequestAsync(string from, string to, BigInteger value)
     {
-        return await contractService.TransferRequestAsync(to, value);
+        return contractService.TransferFromRequestAsync(from, to, value);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.AllowanceQueryAsync(AllowanceFunction, BlockParameter)"/>
-    public virtual async Task<BigInteger> AllowanceQueryAsync(AllowanceFunction allowanceFunction, BlockParameter? blockParameter = null)
+    public virtual Task<TransactionReceipt> TransferFromRequestAndWaitForReceiptAsync(TransferFromFunction transferFromFunction,
+        CancellationToken cancellationToken = default)
     {
-        return await contractService.AllowanceQueryAsync(allowanceFunction, blockParameter);
+        return contractService.TransferFromRequestAndWaitForReceiptAsync(transferFromFunction, cancellationToken);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.AllowanceQueryAsync(string, string, BlockParameter)"/>
-    public virtual async Task<BigInteger> AllowanceQueryAsync(string owner, string spender, BlockParameter? blockParameter = null)
+    public virtual Task<TransactionReceipt> TransferFromRequestAndWaitForReceiptAsync(string from, string to, BigInteger value, CancellationToken cancellationToken = default)
     {
-        return await contractService.AllowanceQueryAsync(owner, spender, blockParameter);
+        return contractService.TransferFromRequestAndWaitForReceiptAsync(from, to, value, cancellationToken);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.AllowedQueryAsync(AllowedFunction, BlockParameter)"/>
-    public virtual async Task<BigInteger> AllowedQueryAsync(AllowedFunction allowedFunction, BlockParameter? blockParameter = null)
+    public virtual Task<BigInteger> GetBalanceFromStorageAsync(string address, BigInteger slot, BlockParameter? blockParameter = null)
     {
-        return await contractService.AllowedQueryAsync(allowedFunction, blockParameter);
+        return contractService.GetBalanceFromStorageAsync(address, slot, blockParameter);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.AllowedQueryAsync(string, string, BlockParameter)"/>
-    public virtual async Task<BigInteger> AllowedQueryAsync(string owner, string spender, BlockParameter? blockParameter = null)
+    public virtual Task<BigInteger> BalancesQueryAsync(BalancesFunction balancesFunction, BlockParameter? blockParameter = null)
     {
-        return await contractService.AllowedQueryAsync(owner, spender, blockParameter);
+        return contractService.BalancesQueryAsync(balancesFunction, blockParameter);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.BalanceOfQueryAsync(BalanceOfFunction, BlockParameter)"/>
-    public virtual async Task<BigInteger> BalanceOfQueryAsync(BalanceOfFunction balanceOfFunction, BlockParameter? blockParameter = null)
+    public virtual Task<BigInteger> BalancesQueryAsync(string address, BlockParameter? blockParameter = null)
     {
-        return await contractService.BalanceOfQueryAsync(balanceOfFunction, blockParameter);
+        return contractService.BalancesQueryAsync(address, blockParameter);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.BalanceOfQueryAsync(string, BlockParameter)"/>
-    public virtual async Task<BigInteger> BalanceOfQueryAsync(string owner, BlockParameter? blockParameter = null)
+    public virtual Task<byte> DecimalsQueryAsync(DecimalsFunction decimalsFunction, BlockParameter? blockParameter = null)
     {
-        return await contractService.BalanceOfQueryAsync(owner, blockParameter);
+        return contractService.DecimalsQueryAsync(decimalsFunction, blockParameter);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.BalancesQueryAsync(BalancesFunction, BlockParameter)"/>
-    public virtual async Task<BigInteger> BalancesQueryAsync(BalancesFunction balancesFunction, BlockParameter? blockParameter = null)
+    public virtual Task<byte> DecimalsQueryAsync(BlockParameter? blockParameter = null)
     {
-        return await contractService.BalancesQueryAsync(balancesFunction, blockParameter);
+        return contractService.DecimalsQueryAsync(blockParameter);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.BalancesQueryAsync(string, BlockParameter)"/>
-    public virtual async Task<BigInteger> BalancesQueryAsync(string owner, BlockParameter? blockParameter = null)
+    public virtual Task<BigInteger> AllowedQueryAsync(AllowedFunction allowedFunction, BlockParameter? blockParameter = null)
     {
-        return await contractService.BalancesQueryAsync(owner, blockParameter);
+        return contractService.AllowedQueryAsync(allowedFunction, blockParameter);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.GetBalanceFromStorageAsync(string, BigInteger, BlockParameter)"/>
-    public virtual async Task<BigInteger> GetBalanceFromStorageAsync(string address, BigInteger slot, BlockParameter? blockParameter = null)
+    public virtual Task<BigInteger> AllowedQueryAsync(string owner, string spender, BlockParameter? blockParameter = null)
     {
-        return await contractService.GetBalanceFromStorageAsync(address, slot, blockParameter);
+        return contractService.AllowedQueryAsync(owner, spender, blockParameter);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.ApproveRequestAndWaitForReceiptAsync(ApproveFunction, CancellationToken)"/>
-    public virtual async Task<TransactionReceipt> ApproveRequestAndWaitForReceiptAsync(ApproveFunction approveFunction, CancellationToken cancellationToken = default)
+    public virtual Task<BigInteger> BalanceOfQueryAsync(BalanceOfFunction balanceOfFunction, BlockParameter? blockParameter = null)
     {
-        return await contractService.ApproveRequestAndWaitForReceiptAsync(approveFunction, cancellationToken);
+        return contractService.BalanceOfQueryAsync(balanceOfFunction, blockParameter);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.ApproveRequestAndWaitForReceiptAsync(string, BigInteger, CancellationToken)"/>
-    public virtual async Task<TransactionReceipt> ApproveRequestAndWaitForReceiptAsync(string spender, BigInteger value, CancellationToken cancellationToken = default)
+    public virtual Task<BigInteger> BalanceOfQueryAsync(string owner, BlockParameter? blockParameter = null)
     {
-        return await contractService.ApproveRequestAndWaitForReceiptAsync(spender, value, cancellationToken);
+        return contractService.BalanceOfQueryAsync(owner, blockParameter);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.TransferFromRequestAndWaitForReceiptAsync(TransferFromFunction, CancellationToken)"/>
-    public virtual async Task<TransactionReceipt> TransferFromRequestAndWaitForReceiptAsync(TransferFromFunction transferFromFunction, CancellationToken cancellationToken = default)
+    public virtual Task<string> TransferRequestAsync(TransferFunction transferFunction)
     {
-        return await contractService.TransferFromRequestAndWaitForReceiptAsync(transferFromFunction, cancellationToken);
+        return contractService.TransferRequestAsync(transferFunction);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.TransferFromRequestAndWaitForReceiptAsync(string, string, BigInteger, CancellationToken)"/>
-    public virtual async Task<TransactionReceipt> TransferFromRequestAndWaitForReceiptAsync(string from, string to, BigInteger value, CancellationToken cancellationToken = default)
+    public virtual Task<string> TransferRequestAsync(string to, BigInteger value)
     {
-        return await contractService.TransferFromRequestAndWaitForReceiptAsync(from, to, value, cancellationToken);
+        return contractService.TransferRequestAsync(to, value);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.TransferRequestAndWaitForReceiptAsync(TransferFunction, CancellationToken)"/>
-    public virtual async Task<TransactionReceipt> TransferRequestAndWaitForReceiptAsync(TransferFunction transferFunction, CancellationToken cancellationToken = default)
+    public virtual Task<TransactionReceipt> TransferRequestAndWaitForReceiptAsync(TransferFunction transferFunction, CancellationToken cancellationToken = default)
     {
-        return await contractService.TransferRequestAndWaitForReceiptAsync(transferFunction, cancellationToken);
+        return contractService.TransferRequestAndWaitForReceiptAsync(transferFunction, cancellationToken);
     }
 
-    /// <inheritdoc cref="ERC20ContractService.TransferRequestAndWaitForReceiptAsync(string, BigInteger, CancellationToken)"/>
-    public virtual async Task<TransactionReceipt> TransferRequestAndWaitForReceiptAsync(string to, BigInteger value, CancellationToken cancellationToken = default)
+    public virtual Task<TransactionReceipt> TransferRequestAndWaitForReceiptAsync(string to, BigInteger value,
+        CancellationToken cancellationToken = default)
     {
-        return await contractService.TransferRequestAndWaitForReceiptAsync(to, value, cancellationToken);
+        return contractService.TransferRequestAndWaitForReceiptAsync(to, value, cancellationToken);
+    }
+
+    public virtual Task<BigInteger> AllowanceQueryAsync(AllowanceFunction allowanceFunction, BlockParameter? blockParameter = null)
+    {
+        return contractService.AllowanceQueryAsync(allowanceFunction, blockParameter);
+    }
+
+    public virtual Task<BigInteger> AllowanceQueryAsync(string owner, string spender, BlockParameter? blockParameter = null)
+    {
+        return contractService.AllowanceQueryAsync(owner, spender, blockParameter);
     }
 }
